7b90f071428af9b030e87b55c73c5b06f0331f26
Ivan Yavorin <just1member@gmail.com>
2016-01-28 11:24:34 +0200

    added header file

---
+++ TaskManager/BoardDetailsHeader.swift
@@ -0,0 +1,21 @@
+//
+//  BoardDetailsHeader.swift
+//  TaskManager
+//
+//  Created by CloudCraft on 1/28/16.
+//  Copyright © 2016 CloudCraft. All rights reserved.
+//
+
+import UIKit
+
+class BoardDetailsHeader: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
