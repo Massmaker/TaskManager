13e512af298165f735ceaa684d9a7a637b25cbf6
Ivan Yavorin <just1member@gmail.com>
2016-01-29 11:36:38 +0200

    added subclass for table view cell

---
+++ TaskManager/ViewControllers/UserTasks/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  TaskManager
+//
+//  Created by CloudCraft on 1/29/16.
+//  Copyright © 2016 CloudCraft. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
